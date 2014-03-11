673c4e10b635b5153fb9ef92151f4917540df9d5
Mikko Kivelä <mkivela@rallydev.com>
2014-03-11 10:46:23 +0200

    Show collage after article images

--- Slideshow.m
+++ Slideshow.m
@@ -5,22 +5,22 @@ conf = config();
 
 %tracker = EyeTracker.instance();
 
-% Lue artikkelikansion sis?lt?
+% Lue artikkelikansion sisältö
 image_dir = dir(conf.image_dir);
 
-% Lue kaikki alakansioiden nimet (yksitt?iset artikkelit)
-% Poista nykyinen ja yl?kansio (., ..)
+% Lue kaikki alakansioiden nimet (yksittäiset artikkelit)
+% Poista nykyinen ja yläkansio (., ..)
 subdirs = [image_dir(:).isdir];
 articles = {image_dir(subdirs).name}; %
 articles(ismember(articles, {'.','..'})) = [];
 
-% Yhdist? seurantakameraan ja kalibroi
-%tracker.initializeTracker();
-%tracker.connect();
-%tracker.calibrate()
+% Yhdistä seurantakameraan ja kalibroi
+tracker.initializeTracker();
+tracker.connect();
+tracker.calibrate()
 
 try
-    % Alusta n?ytt?
+    % Alusta näyttö
     Screen('Preference', 'SkipSyncTests', 1);
     %Screen('Preference', 'ConserveVRAM', 64);
     screens = Screen('Screens');
@@ -33,24 +33,23 @@ try
     %tracker.startRecording();
     
     for article = articles
-        % Lue muistiin t?m?n artikkelin nimi ja kuvat
+        % Lue muistiin tämän artikkelin nimi ja kuvat
         article_dir = article{1};
         article_path = strcat(conf.image_dir, '/', article_dir);
         [title, images, image_names] = load_article(article_path, conf.resolution);
 
-        % N?yt? artikkelin otsikko
+        % Näytä artikkelin otsikko
         show_title(title, screen, window);
         pause(conf.title_show_time);
         
-        %{
         for i = 1:length(images)
-            Screen('PutImage', window, images{i}); % Piirr? kuva puskuriin
-            % L?het? palvelimelle aikamerkki kuvan nimell?
+            Screen('PutImage', window, images{i}); % Piirrä kuva puskuriin
+            % Lähetä palvelimelle aikamerkki kuvan nimellä
             %tracker.setMarker(image_names{i});
-            Screen('Flip', window); % N?yt? puskuroitu kuva
+            Screen('Flip', window); % Näytä puskuroitu kuva
             pause(conf.image_show_time);
         end
-        %}
+        
         show_collage(images, screen, window);
         KbPressWait();
     end
@@ -65,6 +64,6 @@ catch
 end
 
 % Tallenna seurantadata ja sulje yhteys
-%tracker.stopRecording();
-%tracker.saveData(conf.save_path);
-%tracker.disconnect();
+tracker.stopRecording();
+tracker.saveData(conf.save_path);
+tracker.disconnect();
--- show_collage.m
+++ show_collage.m
@@ -1,36 +1,38 @@
 function [] = show_collage( images, screen, window )
 
-per_row = 5;
-per_col = 2;
+  per_row = 5;
+  per_col = 2;
 
-[screen_width, screen_height] = Screen('WindowSize', screen);
-collage_width = screen_width - 200;
-collage_height = screen_height - 200;
-border = 10;
+  [screen_width, screen_height] = Screen('WindowSize', screen);
+  collage_width = screen_width - 200;
+  collage_height = screen_height - 200;
+  border = 10;
 
-image_count = length(images);
-image_size = [collage_height/per_col collage_width/per_row];
+  image_count = length(images);
+  image_size = [collage_height/per_col collage_width/per_row];
 
-% Luo uusi kollaasikuva
-collage = uint8(zeros(collage_height + border * per_col, ...
-    collage_width + border * per_row, 3));
-collage(:,:,:) = 255;
+  % Luo uusi kollaasikuva
+  collage = uint8(zeros(collage_height + border * per_col, ...
+      collage_width + border * per_row, 3));
+  collage(:,:,:) = 255;
 
-for i = 1:image_count
-    % Yhten?ist? kuvan koko kollaasia varten
-    resized_image = imresize(images{i}, image_size);
-    col = mod(i-1, per_row);
-    row = floor((i-1)/per_row);
-    offsety = border * (row + 1);
-    offsetx = border * (col + 1);
+  for i = 1:image_count
+      % Yhtenäistä kuvan koko kollaasia varten
+      resized_image = imresize(images{i}, image_size);
+      col = mod(i-1, per_row);
+      row = floor((i-1)/per_row);
+      offsety = border * (row + 1);
+      offsetx = border * (col + 1);
     
-    % Siirr? kuva kollaasiin
-    collage((image_size(1)*row+offsety+1):(image_size(1)*(row+1)+offsety), ...
-        (image_size(2)*col+offsetx+1):(image_size(2)*(col+1)+offsetx), ...
-        :) = resized_image;
+      % Siirrä kuva kollaasiin
+      collage((image_size(1)*row+offsety+1):(image_size(1)*(row+1)+offsety), ...
+          (image_size(2)*col+offsetx+1):(image_size(2)*(col+1)+offsetx), ...
+          :) = resized_image;
+  end
+
+  Screen('PutImage', window, collage); % Piirrä kuva puskuriin
+  Screen('Flip', window); % Näytä puskuroitu kuva
+
 end
 
-Screen('PutImage', window, collage); % Piirr? kuva puskuriin
-Screen('Flip', window); % N?yt? puskuroitu kuva
-
-end
+function 
